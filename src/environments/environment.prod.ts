5605f42cbf7fdf27206a913a83b7c7a63e333f44
pgzubizarreta <pgzubizarreta@fp.lasalle.es>
2026-04-23 10:21:15 +0200

    Anadir configuracion de Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyB0Nd2IBx7mION9-_4b8AFTkqrCfdA_Gkc',
+    authDomain: 'campus-quest-59056.firebaseapp.com',
+    projectId: 'campus-quest-59056',
+    storageBucket: 'campus-quest-59056.firebasestorage.app',
+    messagingSenderId: '255076470385',
+    appId: '1:255076470385:web:961386abfcdcdcb33695d0',
   },
 };
